e11be0e90c3aed77292ad69e7f46a239195cccf1
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2022-03-23 21:32:30 +0530

    ui fixed for cards

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Snackbar from "@mui/material/Snackbar";
+import Slide from "@mui/material/Slide";
+import Alert from "@mui/material/Alert";
 
 import Styles from "./Card.module.css";
 
 const Card = (props) => {
+  const [open, setopen] = useState(false);
+  const [transition, setTransition] = useState(undefined);
+
+  const TransitionUp = (props) => {
+    return <Slide {...props} direction="up" />;
+  };
+
+  const addToCartHandler = () => {
+    setTransition(() => TransitionUp);
+    setopen(true);
+  };
+
+  const handleClose = () => {
+    setopen(false);
+  };
+
   return (
     <div className={Styles.card}>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        autoHideDuration={4000}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={transition}
+        key={transition ? transition.name : ""}
+      >
+        <Alert severity="success">
+          Item Added to Cart
+        </Alert>
+      </Snackbar>
       <div>
         <img
           src={props.details.img}
@@ -14,13 +45,13 @@ const Card = (props) => {
         />
       </div>
       <div className={Styles.descCont}>
-        <p>{props.details.name}</p>
         <div className={Styles.desc}>
+          <p>{props.details.name}</p>
           <div className={Styles.price}>${props.details.price}</div>
-          <div className={Styles.view}>
-            <Link to="/">Add To Cart</Link>
-          </div>
         </div>
+        <button className={Styles.view} onClick={addToCartHandler}>
+          Add To Cart
+        </button>
       </div>
     </div>
   );
